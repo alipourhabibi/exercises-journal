e71da8b0b5970cbd37e48b8704cf795333758c9e
Ali Pourhabibi <ali.pourhabibi1379@gmail.com>
2023-11-08 19:41:50 +0330

    fix(logger): fix logger Leveling orders
    fix logger leveling orders base on verboseness

--- logging/logger/logger.go
+++ logging/logger/logger.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"time"
 )
 
 var output = os.Stdout
@@ -11,8 +12,8 @@ var output = os.Stdout
 type Level int
 
 var (
-	LevelDebug Level = 8
-	LevelInfo  Level = 4
+	LevelDebug Level = 4
+	LevelInfo  Level = 3
 	LevelWarn  Level = 2
 	LevelError Level = 1
 )
@@ -24,49 +25,54 @@ var levelStr = map[Level]string{
 	LevelError: "[ERROR]",
 }
 
-type logger struct {
+type Logger struct {
 	prefix string
 	level  Level
 }
 
-func New(output *os.File, prefix string, level Level) *logger {
-	output = output
-	return &logger{
+func New(out *os.File, prefix string, level Level) *Logger {
+	output = out
+	return &Logger{
 		prefix: prefix,
 		level:  level,
 	}
 }
 
-func (l *logger) SetLever(level Level) {
+func (l *Logger) SetLevel(level Level) {
 	l.level = level
 }
 
-func (l *logger) SetOutput(out *os.File) {
+func (l *Logger) GetLevel() Level {
+	return l.level
+}
+
+func (l *Logger) SetOutput(out *os.File) {
 	output = out
 }
 
-func (l *logger) log(ctx context.Context, level Level, msg string, args ...any) {
+func (l *Logger) log(ctx context.Context, level Level, msg string, args ...any) {
 	strs := []string{}
 	for _, v := range args {
 		strs = append(strs, fmt.Sprintf("%s", v))
 	}
 	msg = fmt.Sprintf(msg, args...)
-	msg = levelStr[level] + " " + l.prefix + " " + msg + "\n"
+	msg = fmt.Sprintf("%s %s %s", levelStr[level], l.prefix, msg)
+	msg = fmt.Sprintf("[%s] %s\n", time.Now().Format("2006-01-02 15:04:05"), msg)
 	output.Write([]byte(msg))
 }
 
-func (l *logger) Info(msg string, args ...any) {
+func (l *Logger) Info(msg string, args ...any) {
 	l.log(context.Background(), LevelInfo, msg, args...)
 }
 
-func (l *logger) Debug(msg string, args ...any) {
+func (l *Logger) Debug(msg string, args ...any) {
 	l.log(context.Background(), LevelDebug, msg, args...)
 }
 
-func (l *logger) Warn(msg string, args ...any) {
-	l.log(context.Background(), LevelDebug, msg, args...)
+func (l *Logger) Warn(msg string, args ...any) {
+	l.log(context.Background(), LevelWarn, msg, args...)
 }
 
-func (l *logger) Error(msg string, args ...any) {
-	l.log(context.Background(), LevelDebug, msg, args...)
+func (l *Logger) Error(msg string, args ...any) {
+	l.log(context.Background(), LevelError, msg, args...)
 }
